570a14d9ac8a79dd5715eda9365b0fc8474459b6
Materuli <56313808+Materuli@users.noreply.github.com>
2021-03-17 17:26:09 -0700

    Creating Base Project
    Includes a couple of rooms, walls appearing and disappearing

---
+++ Assets/Scripts/CubeFollower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFollower : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset = new Vector3(-5, 3);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
