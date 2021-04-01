2f5d79d0c5cbb6c09d7845bcba4b3f07cefbc7b4
HammondEggs1 <zhammond10643@yahoo.com>
2021-04-01 10:10:47 -0700

    Uploaded Project 1
    First full project upload

---
+++ Controlled Character/Assets/projectileController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileController : MonoBehaviour
+{
+    public Camera cam;
+    public Transform LHFirePoint, RHFirePoint;
+    public GameObject projectile;
+    public float projectileSpeed = 30f;
+    private Vector3 destination;
+    private bool lefthand;
+    private float timeToFire;
+    public float fireRate = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //fires projectiles
+        if (Input.GetButton ("Fire1") && Time.time >= timeToFire)
+        {
+            timeToFire = Time.time + 1 / fireRate;
+            ShootProjectile();
+        }
+    }
+    void ShootProjectile()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            destination = hit.point;
+        }
+        else
+        {
+            destination = ray.GetPoint(1000);
+        }
+        if (lefthand)
+        {
+            lefthand = false;
+            InstantiateProjectile(LHFirePoint);
+        }
+        else
+        {
+            lefthand = true;
+            InstantiateProjectile(RHFirePoint);
+        }
+
+    }
+
+    void InstantiateProjectile(Transform firePoint)
+    {
+        var projectileObj = Instantiate(projectile, firePoint.position, Quaternion.identity) as GameObject;
+        projectileObj.GetComponent<Rigidbody>().velocity = (destination - firePoint.position).normalized * projectileSpeed;
+    }
+}
+
---
+++ Controlled Character/Assets/projectileMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileMover : MonoBehaviour
+{
+    private bool collided;
+    public GameObject impactVFX;
+    void OnCollisionEnter(Collision co)
+    {
+        if(co.gameObject.tag != "Bullet" && co.gameObject.tag != "Player" && !collided)
+        {
+            collided = true;
+            var impact = Instantiate(impactVFX, co.contacts[0].point, Quaternion.identity) as GameObject;
+            Destroy(impact, 2);
+            Destroy(gameObject);
+        }
+    }
+}
